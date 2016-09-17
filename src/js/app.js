56df60fab9f7c9f88cb01cd8f7e518f6b4aef073
Matthew <m45yang@uwaterloo.ca>
2016-09-17 17:50:52 -0400

    fix dependency

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom'
 import Header from './components/header.js'
 import Video from './components/video.js'
+import SongList from './components/songlist.js'
 
 ReactDOM.render(
   <div>
