ffb1eaa16835050744ad93f27706f184c21fe4ca
ttrzeng <talent.z@Hotmail.com>
2016-09-17 13:59:53 -0400

    added songlist

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -4,9 +4,9 @@ class Header extends React.Component {
   render() {
     return (
       <nav className="light-blue lighten-1" role="navigation">
-        <div className="nav-wrapper container"><a id="logo-container" href="#" className="brand-logo">Hack the North 2016</a>
+        <div className="nav-wrapper container"><a id="logo-container" href="#" className="brand-logo">MoodSwing</a>
           <ul className="right hide-on-med-and-down">
-            <li><a href="#">Navbar Link</a></li>
+            <li><a href="#">Menu</a></li>
           </ul>
         </div>
       </nav>
