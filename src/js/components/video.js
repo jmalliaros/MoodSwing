2333459b0be13821b3ce8a3cb91a276b80721278
Matthew <m45yang@uwaterloo.ca>
2016-09-17 17:48:04 -0400

    fuck this shit

--- src/js/components/video.js
+++ src/js/components/video.js
@@ -1,12 +1,13 @@
-import Webcam from 'react-webcam'
 import React, { Component, PropTypes } from 'react'
-import TimerMixin from 'react-timer-mixin';
+import TimerMixin from 'react-timer-mixin'
+import jic from 'j-i-c'
 
 class Video extends React.Component {
   constructor(props) {
     super(props)
 
     this.snap = this.snap.bind(this)
+    this.upload = this.upload.bind(this)
     this.snapInterval = 5000
   }
 
@@ -17,9 +18,9 @@ class Video extends React.Component {
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices.getUserMedia({ video: true })
       .then(function(stream) {
-        video.src = window.URL.createObjectURL(stream);
-        video.play();
-      });
+        video.src = window.URL.createObjectURL(stream)
+        video.play()
+      })
     }
 
     TimerMixin.setInterval(this.snap, this.snapInterval)
@@ -29,9 +30,51 @@ class Video extends React.Component {
     var video = this.refs.video
     var canvas = this.refs.canvas
     var context = canvas.getContext('2d')
-    var button = this.refs.snapButton
 
-    context.drawImage(video, 0, 0, 640, 480)
+    context.drawImage(video, 0, 0, 0, 0)
+    var dataURI = canvas.toDataURL('img/jpeg')
+
+    this.upload(dataURI)
+
+    // $.ajax({
+    //   method: 'POST',
+    //   url: 'http://localhost:5000/images',
+    //   headers: {
+    //     'Content-Type': 'application/octet-stream'
+    //   },
+    //   data: dataURI,
+    //   success: function(data, status) {
+    //     console.log(data)
+    //     console.log(status)
+    //   },
+    //   error: function(xhr, desc, err) {
+    //     console.log(xhr)
+    //     console.log("Desc: " + desc + "\nErr:" + err)
+    //   }
+    // })
+  }
+
+  upload(data) {    
+    var target_img = this.refs.target_img
+    var quality = 80
+    var output_format = 'jpg'
+
+    console.log(jic)
+    target_img.src = jic.compress(data, quality, output_format).src  
+     
+    var server_endpoint = 'http://localhost:5000/images'
+    var server_var_name = 'file'
+    var filename = "new.jpg"
+     
+    var successCallback = function(response){ console.log(response) }
+     
+    var errorCallback = function () {
+    }
+     
+    var duringCallback = function (progressPercent) {
+    }
+     
+    jic.upload(target_img, server_endpoint, server_var_name, filename, successCallback, errorCallback, duringCallback, customHeaders)  
   }
 
   render() {
@@ -44,6 +87,7 @@ class Video extends React.Component {
           <div className="col s6">
             <canvas ref="canvas" width="640" height="480"></canvas>
           </div>
+          <img ref="target_img"></img>
         </div>
       </div>
     )
